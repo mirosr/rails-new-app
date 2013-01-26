60157265ac7e271dd77d99d30366c9abbe6cdb19
Miroslav Rachev <miro@mirosr.net>
2013-01-26 18:03:30 +0200

    Added a hint for manual db creation in the base template

--- base.rb
+++ base.rb
@@ -65,4 +65,6 @@ git commit: '-aqm "initial commit"'
 say %q{
         done    loading template
       =============================================================
+
+Hint: You should create the application's database manually with rake db:create
 }
